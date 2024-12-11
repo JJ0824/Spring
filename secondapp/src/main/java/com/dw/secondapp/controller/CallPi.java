d63bfbbec28347acb987a7e90074a41edced8905
JJ0824 <auxjungjae@gmail.com>
2024-12-11 16:52:36 +0900

    update app

--- secondapp/src/main/java/com/dw/secondapp/controller/CallPi.java
+++ secondapp/src/main/java/com/dw/secondapp/controller/CallPi.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-    @GetMapping("/makenew")
-    public String MakeNew() {
-        return "New";
+public class CallPi {
+    @GetMapping("/callpi")
+    public String Pi() {
+        return (" "+Math.PI);
     }
 }
