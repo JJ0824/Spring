6b91089bb120117283ce39369ee88a77b42bb514
JJ0824 <auxjungjae@gmail.com>
2024-12-18 13:48:58 +0900

    update

---
+++ Exception/src/Exception_2.java
@@ -0,0 +1,22 @@
+public class Exception_2 {
+    public static void main(String[] args) {
+        // 다중 try~catch문
+        try {
+            System.out.println(3/1);
+            int num = Integer.parseInt("10A"); // NumberFormatException
+            int[] array = new int[3];
+            array[3]=0; // ArrayIndexOutOfBoundsEx
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+            System.out.println("0으로 나눌 수 없음");
+        } catch (NumberFormatException e) {
+            System.out.println("숫자가 아닙니다");
+        }
+        // 예상 가능한 예외는 catch문을 통해서 예외처리를  해주는 게 원칙이나,
+        // 만약 예상하지 못한 예외의 가능성이 있다면 제일 아래에 부모예외클래스를
+        // 추가해주어 모든 예외를 처리해줄 수 있음
+        catch (RuntimeException e) {
+            System.out.println("기타 예외 발생.. 하지만 난 처리했음");
+        }
+    }
+}
